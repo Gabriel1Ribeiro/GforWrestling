8316a3fa24b7ba72e6ae325f622462071f87ce72
Gabriel1Ribeiro <gabriel.hribeiro@sptech.school>
2025-05-12 06:26:27 -0300

    1/3 das telas das brands (Raw, SmackDown e Nxt), fiz até os triângulos

--- web-data-viz/public/js/navegandoTelas.js
+++ web-data-viz/public/js/navegandoTelas.js
@@ -5,3 +5,13 @@ function Irlogin() {
 function IrHomePage() {
     window.location.href = "homePage.html";
 }
+
+function IrRaw() {
+    window.location.href = "raw.html";
+}
+function IrSmackDown() {
+    window.location.href = "smackDown.html";
+}
+function IrNxt() {
+    window.location.href = "nxt.html";
+}
